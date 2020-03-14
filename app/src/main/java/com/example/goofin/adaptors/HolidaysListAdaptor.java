ba2ad68b01d71a16cb7006c7944cb0e292af01d3
Matthew Barber <quitesimplymatt@gmail.com>
2020-03-14 11:09:38 +0000

    crop thumbnails

--- app/src/main/java/com/example/goofin/adaptors/HolidaysListAdaptor.java
+++ app/src/main/java/com/example/goofin/adaptors/HolidaysListAdaptor.java
@@ -3,6 +3,7 @@ package com.example.goofin.adaptors;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -59,6 +60,7 @@ public class HolidaysListAdaptor extends RecyclerView.Adapter<HolidaysListAdapto
 
     public HolidaysListAdaptor(Context context) {
         layoutInflater = LayoutInflater.from(context);
+
     }
 
     @Override
@@ -88,7 +90,7 @@ public class HolidaysListAdaptor extends RecyclerView.Adapter<HolidaysListAdapto
 
                     viewHolder.imageView.setImageBitmap(imageBm);
                 } catch (NoSuchElementException e) {
-                    
+
                 }
             }
         }
